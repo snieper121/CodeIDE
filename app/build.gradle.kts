4db3ae2e1761490e0fbdbd26c84274bdbe30472e
snieper121 <mredozubovlist@gmail.com>
2025-09-24 15:44:10 +0300

    test cursor

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,7 +72,6 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
-    implementation "androidx.documentfile:documentfile:1.0.1"
 
     // Тестовые зависимости
     testImplementation("junit:junit:4.13.2")
